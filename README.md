44807bdca19e019c4cf4ac082a7a0b79cecd72d3
amrs-tech <amrs.tech@gmail.com>
2019-05-01 22:52:23 +0530

    Updated the program

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 ## Automated method to check your personal mail
 
-This automation is done using Python along with the Selenium package.
+This automation is done using Python 3.x with the Selenium package.
 
 PreRequisites are:
   
-  * Python IDLE
+  * Python IDLE (or any IDE)
   * Selenium Package
   * Chrome Webdriver (in case of Chrome)
   * Geckodriver (in case of Firefox)
-  * Respective WebBrowser
+  * Respective WebBrowser with matching version
 
 ![Screenshot](/seleniumOP.jpg)
--- selProject.py
+++ selProject.py
@@ -1,32 +1,45 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
-driver = webdriver.Firefox() #initializing the firefox webdriver and open browser
+driver = webdriver.Chrome('/Users/amrs/Downloads/chromedriver') #initializing the chrome webdriver and open browser
 driver.get('https://mail.google.com') #open the website mentioned
 try:
     link = driver.find_element_by_link_text('Sign In')
     link.click()
 except:
-    sleep(2)
+    sleep(1)
     
-email = driver.find_element_by_id('Email') #find the text field to enter email id
+email = driver.find_element_by_id('identifierId') #find the input field to enter email id
 
-email.send_keys('username@gmail.com') #type in your required mail id
+email.send_keys('your_mail_id_@gmail.com') #type in your required mail id
 
-nxt = driver.find_element_by_id('next')
-nxt.click() #click the next button
+nxt = driver.find_element_by_id('identifierNext')
+nxt.click() #click the next button for this email id
 
 sleep(1)
 
-password = driver.find_element_by_id('Passwd') #find password text field
+password = driver.find_element_by_name('password') #find password input field
 
-password.send_keys('password') #type in your required password
+password.send_keys('*********') #type in your required password
 sleep(1)
-driver.find_element_by_id('PersistentCookie').click() #to deselect "keep me signed in" option
-sleep(1)
-sign = driver.find_element_by_id('signIn') #find the sign in button
-sign.click()
+driver.find_element_by_id('passwordNext').click() #to signin with this password option
+sleep(3)
 
+#wait till the email is open and signout option is visible
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//*[@id='gb']/div[2]/div[3]/div/div[2]/div/a"))
+    )
+finally:
+	sleep(1)
+	driver.find_element_by_xpath("//*[@id='gb']/div[2]/div[3]/div/div[2]/div/a").click()
+	sleep(1)
+	driver.find_element_by_id('gb_71').click()
+
+sleep(1)
 driver.close()
 
